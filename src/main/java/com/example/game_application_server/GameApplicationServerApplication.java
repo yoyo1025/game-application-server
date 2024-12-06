c4fd6719f579ef93413f4a6d88cf59e43f6e2676
yoyo <hsjejxys@gmail.com>
2024-12-06 22:43:58 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/game_application_server/GameApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_application_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplicationServerApplication.class, args);
+	}
+
+}
