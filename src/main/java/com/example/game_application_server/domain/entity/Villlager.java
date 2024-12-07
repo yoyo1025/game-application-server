23f22d42abe701f9b888c42659ddbc8ea62df841
yoyo <hsjejxys@gmail.com>
2024-12-07 15:09:27 +0900

    Villagerクラスを実装

--- src/main/java/com/example/game_application_server/domain/entity/Villlager.java
+++ src/main/java/com/example/game_application_server/domain/entity/Villlager.java
@@ -1,4 +1,40 @@
 package com.example.game_application_server.domain.entity;
 
-public class Villlager {
+public class Villlager extends Player{
+
+    // 村人が所持するポイント。初期値はゼロ。
+    public int points;
+
+    // 生死情報を表す。初期値はtrueで鬼に捕まるとfalseになる。
+    public boolean isAlive;
+
+    public Villlager(int id, String name, boolean isConnected, boolean isOnBreak) {
+        super(id, name, isConnected, isOnBreak);
+        this.points = 0;
+        this.isAlive = true;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+
+    public void addPoints() {
+        this.points += 1;
+    }
+
+    public boolean isAlive() {
+        return this.isAlive;
+    }
+
+    public void setAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    @Override
+    public String toString() {
+        return "Villager { id = " + super.id + ", name = " + super.name +
+                ", isConnected = " + super.isConnected + ", isOnBreak = " + super.isOnBreak +
+                ", points = " + this.points + ", isAlive = " + this.isAlive + " }";
+    }
+
 }
