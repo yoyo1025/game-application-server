ed8cad3ef5d7e6c2c6402b03c70e7ad2f2e40230
yoyo <hsjejxys@gmail.com>
2024-12-21 16:34:33 +0900

    プレイヤーデータの初期化用エンドポイントを作成

--- src/main/java/com/example/game_application_server/GameApplicationServerApplication.java
+++ src/main/java/com/example/game_application_server/GameApplicationServerApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GameApplicationServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GameApplicationServerApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/game_application_server/presentation/GameController.java
+++ src/main/java/com/example/game_application_server/presentation/GameController.java
@@ -1,4 +1,26 @@
 package com.example.game_application_server.presentation;
 
+import com.example.game_application_server.dto.PlayerInfo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class GameController {
+
+    @PostMapping("/init-player-info")
+    public ResponseEntity<List<PlayerInfo>> initPlayerInfo(@RequestBody List<PlayerInfo> playersInfo) {
+        // 受け取ったデータをログに出力
+        playersInfo.forEach(player -> System.out.println(
+                "userId: " + player.getUserId() + ", userName: " + player.getUserName() + ", isDemon: " + player.isDemon()
+        ));
+
+        // すべてのプレイヤー情報をレスポンスとして返す
+        return ResponseEntity.ok(playersInfo);
+    }
 }
