6347aff4e51a677c66737f2209443172c50ef64b
yoyo <hsjejxys@gmail.com>
2025-01-11 12:52:37 +0900

    ターンのスキップ処理を修正

--- src/main/java/com/example/game_application_server/application/SkipTurnUsecase.java
+++ src/main/java/com/example/game_application_server/application/SkipTurnUsecase.java
@@ -1,6 +1,7 @@
 package com.example.game_application_server.application;
 
 import com.example.game_application_server.domain.entity.Player;
+import com.example.game_application_server.domain.entity.Villager;
 import com.example.game_application_server.domain.service.GameState;
 import org.springframework.stereotype.Service;
 
@@ -27,10 +28,32 @@ public class SkipTurnUsecase {
         if (targetPlayer.isOnBreak) {
             throw new IllegalStateException("既に一回休み状態です。");
         }
-            System.out.println("target player: "+targetPlayer.getName());
-            targetPlayer.setOnBreak(true);
-            System.out.println(targetPlayer.getName()+"'s next turn is skipped");
+        System.out.println("target player: "+targetPlayer.getName());
+        targetPlayer.setOnBreak(true);
+        gameState.turn.nextPlayerIndex();
 
+        // 死亡していない&休憩中でないプレイヤーを次のターンのプレイヤーとして選出する
+        int skipCount = 0;
+        int maxPlayers = gameState.players.size();
+        while (skipCount < maxPlayers) {
+            // ---- ここからが「死んだプレイヤーのターンをスキップ」する処理 ----
+            Player nextPlayer = gameState.players.get(gameState.turn.getCurrentPlayerIndex() - 1);
+            // 次のプレイヤーが村人 かつ その村人が死んでいる場合はそのプレイヤーのターンをスキップ
+            if (nextPlayer instanceof Villager && !((Villager) nextPlayer).isAlive()) {
+                gameState.turn.nextPlayerIndex();
+                skipCount++;
+            } else if (nextPlayer.isOnBreak) {
+                System.out.println(nextPlayer.getName() + " is currently on break, skipping...");
+                nextPlayer.setOnBreak(false);  // 次のターンではプレイできるようにフラグをリセット
+                gameState.turn.nextPlayerIndex();
+                skipCount++;
+            } else {
+                // 生きている村人 or 鬼ならターンを確定
+                break;
+            }
+        }
+
+        System.out.println(targetPlayer.getName()+"'s next turn is skipped");
         return gameState;
     }
 }
